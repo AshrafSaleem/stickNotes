b07284cf75860adc7978c61b9d8027e3b68454cf
Ashraf <SES160001@student.iumw.edu.my>
2019-01-01 13:34:24 +0800

    Added Console Line Arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main()
+using namespace std;
+
+//Command line arguments
+int main(int argc, char *argv[])
 {
+    cout << argc << endl;
+
+    
+    for(size_t i = 0; i < argc; i++)
+    {
+        cout << argv[i]  << endl;
+    }
     
 }
